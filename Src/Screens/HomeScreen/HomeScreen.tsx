ace3d7413e63f8a10a802fe0680b3403761c5e2f
abdrhxyii <abdrhxyii@git>
2024-07-08 15:23:25 +0530

    added bottom sheet in the home page filter section

--- Src/Screens/HomeScreen/HomeScreen.tsx
+++ Src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,80 +1,53 @@
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, StyleSheet, SafeAreaView } from 'react-native';
-import React, {useState} from 'react'
 import Header from '../../Components/HeaderComponent/Header';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import Modal from 'react-native-modal';
-import { PanGestureHandler } from 'react-native-gesture-handler';
-import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 import LabelScroller from '../../Components/LabelScroller/LabelScroller';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import Modal from 'react-native-modal';
+import PrimaryButton from '../../Components/PrimaryButton/PrimaryButton';
+import Slider from '@react-native-community/slider';
 
 const images = {
   img1: require("../../../assets/trousere.jpeg"),
   img2: require("../../../assets/tshirt2.jpg"),
   img3: require("../../../assets/tshirt.jpg")
-}
+};
+
 const productData = [
   { id: 1, img: images.img1, name: 'Regular Fit Slogan', price: '1,190' },
-  { id: 2, img: images.img2,  name: 'Regular Fit Polo', price: '1,100', discount: '52%' },
+  { id: 2, img: images.img2, name: 'Regular Fit Polo', price: '1,100', discount: '52%' },
   { id: 3, img: images.img3, name: 'Regular Fit Black', price: '1,190' },
-  { id: 4, img: images.img3, name: 'Regular Fit V-Neck', price: '1,290' },
-  { id: 5, img: images.img3, name: 'Regular Fit Slogan', price: '1,190' },
-  { id: 6, img: images.img3,  name: 'Regular Fit Polo', price: '1,100', discount: '52%' },
-  { id: 7, img: images.img3, name: 'Regular Fit Black', price: '1,190' },
-  { id: 8, img: images.img3, name: 'Regular Fit V-Neck', price: '1,290' },
-  { id: 9, img: images.img3, name: 'Regular Fit Slogan', price: '1,190' },
-  { id: 10, img: images.img3,  name: 'Regular Fit Polo', price: '1,100', discount: '52%' },
-  { id: 11, img: images.img3, name: 'Regular Fit Black', price: '1,190' },
-  { id: 12, img: images.img3, name: 'Regular Fit V-Neck', price: '1,290' }
 ];
 
-  // const translateY = useSharedValue(0);
+const HomeScreen = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const [price, setPrice] = useState(0);
 
 
-  const HomeScreen = () => {
-    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-    const [isModalVisible, setModalVisible] = useState(false);
+  const handleProductDetailNavigation = () => {
+    navigation.navigate("ProductDetail")
+  };
 
-    const toggleModal = () => {
-      setModalVisible(!isModalVisible);
-    };
-
-    const handleProductDetailNavigation = () => {
-      navigation.navigate("ProductDetail")
-    }
-
-  // const gestureHandler = (event: any) => {
-  //   translateY.value = event.nativeEvent.translationY;
-  // };
-
-  // const gestureEndHandler = () => {
-  //   if (translateY.value > 150) {
-  //     setModalVisible(false);
-  //   }
-  //   translateY.value = withSpring(0);
-  // };
-
-  // const animatedStyle = useAnimatedStyle(() => {
-  //   return {
-  //     transform: [{ translateY: translateY.value }],
-  //   };
-  // });
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Header showLeftIcon={false} loggedIn="false" leftIcon="arrow-left" rightIcon="bell" centerText="Discover" lefticonvisible="true"/> */}
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder="Write your message..."
+          placeholder="Search rochell..."
         />
-        <TouchableOpacity style={styles.sendButton} onPress={toggleModal}>
-          <Icon name="sliders-h" color={'white'} size={20} solid={false}/>
+        <TouchableOpacity style={styles.filterBtn} onPress={toggleModal}>
+          <Icon name="sliders-h" color={'white'} size={20} solid={false} />
         </TouchableOpacity>
       </View>
 
-      <LabelScroller itemLabel={["All", "Jeans", "Shoes", "Abayas", "Shalwar", "Bangles", "Underwears"]}/>
+      <LabelScroller itemLabel={["All", "Jeans", "Shoes", "Abayas", "Shalwar", "Bangles", "Underwears"]} />
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.productRow}>
@@ -90,11 +63,59 @@ const productData = [
           ))}
         </View>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
 
-export default HomeScreen
+      <Modal
+        isVisible={isModalVisible}
+        onBackdropPress={toggleModal}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
+        backdropTransitionOutTiming={0}
+        style={styles.modal}
+      >
+        <View style={styles.modalContent}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalText}>Filters</Text>
+            <TouchableOpacity style={styles.closeButton} onPress={toggleModal}>
+              <Icon name="times" size={24} color="#000" />
+            </TouchableOpacity>  
+          </View>  
+          <View style={styles.mainModalContent}>
+            <View style={[styles.sectionWrapper, {height: 100,marginBottom: 10}]}>
+            <Text style={styles.modalTextHeader}>Sort By</Text>
+            <LabelScroller itemLabel={["Relevance", "Price: Low - High", "Price: High - low", "Category: Men", "Category: Women"]} />
+            </View>
+            
+            <View style={[styles.sectionWrapper, {height: 70}]}>
+            <View style={styles.priceContainer}>
+              <Text style={styles.modalTextHeader}>Price</Text>
+              <Text style={styles.sliderValue}>$0 - {price}</Text>
+            </View>
+
+            <View style={styles.sliderContainer}>
+              <Slider
+                minimumValue={0}
+                maximumValue={100}
+                step={1}
+                value={price}
+                onValueChange={setPrice}
+                minimumTrackTintColor="#000000"
+                maximumTrackTintColor="#000000"
+                thumbTintColor="#000000"
+              />
+            </View>
+            </View>
+            
+            <View style={styles.primaryButtonWrapper}>
+            <PrimaryButton text={'Apply Filter'} width={'100%'}/>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -117,19 +138,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: '#f9f9f9',
   },
-  sendButton: {
+  filterBtn: {
     marginLeft: 10,
     backgroundColor: '#000',
     paddingVertical: 12,
     paddingHorizontal: 15,
     borderRadius: 7,
   },
-  sendButtonText: {
+  filterBtnText: {
     color: '#fff',
   },
   categoryContainer: {
     flexDirection: 'row',
-    // marginBottom: 10, // Adjust this value based on your design needs
     paddingTop: 10,
   },
   categoryButton: {
@@ -152,13 +172,11 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   productContainer: {
-    width: '48%', // Adjust this for two columns with some space between them
+    width: '48%',
     height: 260,
-    // borderWidth: 1,
-    // borderColor: '#808080',
     borderRadius: 12,
     marginBottom: 8,
-    overflow: 'hidden', // Ensure contents don't overflow
+    overflow: 'hidden',
   },
   productImage: {
     width: '100%',
@@ -177,7 +195,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginHorizontal: 10,
     textAlign: 'left',
-    marginTop: '10%', // Adjust based on image height
+    marginTop: '10%',
   },
   productPrice: {
     fontSize: 16,
@@ -190,21 +208,61 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     margin: 0,
   },
+  modalHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 1,
+    height: 45
+  },
   modalContent: {
     backgroundColor: 'white',
-    padding: 22,
     borderTopLeftRadius: 17,
     borderTopRightRadius: 17,
     height: '50%',
-    alignItems: 'center',
+    padding: 20
   },
   closeButton: {
-    position: 'absolute',
-    top: 15,
-    right: 15,
+    paddingHorizontal: 7,
+    paddingVertical: 3,
+    borderRadius: 50,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  mainModalContent: {
+    marginTop: 10,
   },
   modalText: {
     fontSize: 20,
     fontWeight: 'bold',
   },
+  sliderContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  modalTextHeader: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    paddingBottom: 10
+  },
+  sliderValue: {
+    color: '#808080',
+    fontWeight: 'bold',
+    fontSize: 14
+  },
+  priceContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  primaryButtonWrapper: {
+    position: 'absolute',
+    bottom: -150,
+    width: '100%'
+  },
+  sectionWrapper: {
+    borderBottomColor: "#ddd",
+    borderBottomWidth: 1
+  }
 });
