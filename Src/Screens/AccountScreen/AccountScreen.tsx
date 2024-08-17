8959b6454a1d3e201a0513045fcb01242a86939f
abdrhxyii <abdrhxyii@git>
2024-08-17 08:34:14 +0530

    account page updates

--- Src/Screens/AccountScreen/AccountScreen.tsx
+++ Src/Screens/AccountScreen/AccountScreen.tsx
@@ -1,34 +1,33 @@
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, SafeAreaView, Image } from 'react-native';
 import React, {useState} from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome5';
+// import Icon from 'react-native-vector-icons/FontAwesome5';
 import PrimaryButton from '../../Components/PrimaryButton/PrimaryButton';
 import Header from '../../Components/HeaderComponent/Header';
+import { Package } from 'lucide-react-native';
 
 const accountItem = [
-  {id: '1', icon: 'box-open', name: "My Orders"},
-  {id: '2', icon: 'user-circle', name: "My Details"},
-  {id: '3', icon: 'home', name: "Address Book"},
-  {id: '4', icon: 'money-check-alt', name: "Payment Methods"},
-  {id: '5', icon: 'bell', name: "Notifications"},
-  {id: '6', icon: 'question-circle', name: "FAQs"},
-  {id: '7', icon: 'headphones-alt', name: "Help Center"},
-  {id: '8', icon: 'sign-out-alt', name: "Logout"}
+  {id: '1', icon: require("../../../assets/package.png"), name: "My Orders"},
+  {id: '2', icon: require("../../../assets/Details.png"), name: "My Details"},
+  {id: '3', icon: require("../../../assets/Address.png"), name: "Address Book"},
+  {id: '4', icon: require("../../../assets/Vector.png"), name: "Payment Methods"},
+  {id: '5', icon: require("../../../assets/Bell.png"), name: "Notifications"},
+  {id: '6', icon: require("../../../assets/Question.png"), name: "FAQs"},
+  {id: '7', icon: require("../../../assets/Headphones.png"), name: "Help Center"},
+  {id: '8', icon: require("../../../assets/package.png"), name: "Logout"}
 ]
 const AccountScreen = () => {
 
   const renderAccountItem = ({item}: any) => (
-    // <View style={styles.itemContainer}>
     <TouchableOpacity style={styles.itemContainer}>
-      <Icon name={item.icon} style={styles.itemIcon} size={18} color="black" solid={false} />
+      <Package size={24} style={styles.itemIcon} />
+      {/* <Image source={item.icon}/> */}
       <Text>{item.name}</Text>
-      <Icon name="box-open" size={10} color="black" solid={false} />
     </TouchableOpacity>
-    // </View>
   )
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header loggedIn="true" leftIcon="arrow-left" rightIcon="bell" centerText="Account"/>
+      <Header loggedIn="true" leftIcon ="arrow-left" rightIcon="bell" centerText="Account"/>
       <FlatList
          data={accountItem}
          renderItem={renderAccountItem}
@@ -54,7 +53,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingVertical: 18,
     alignItems: 'center',
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: '#ddd'
   },
   itemIcon: {
